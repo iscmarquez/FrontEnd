f4fbe1a4ca7a5a6754d5800888b5a72b724f1d07
pawlosierra <pawlosierra@hotmail.com>
2020-10-25 13:04:07 -0400

    Rapport Inscription et Programme

--- frontjs/fonctRapports.js
+++ frontjs/fonctRapports.js
@@ -7,10 +7,18 @@ $(document).ready(function (){
      event.preventDefault();
      processData();
  });
+
+ $("#formulaire2").submit(function(event) {
+   // Prevent the form from submitting via the browser
+   console.log("Submit!!");
+   event.preventDefault();
+   processData2();
+});
+
 });
 
 function processData(){
-
+//console.log(x)
 // const form2 = $("#formulaire");
 const form = document.getElementById("formulaire")
 console.log("FORM: ", form, "TYPE:",typeof(form));
@@ -19,8 +27,19 @@ var data = {};
    data.debut = form.debut.value;
    data.fin = form.fin.value;
 submitForm(data);
-
 }
+
+function processData2(){
+   //console.log(x)
+   // const form2 = $("#formulaire");
+   const form = document.getElementById("formulaire2")
+   console.log("FORM: ", form, "TYPE:",typeof(form));
+   
+   var data = {};
+      data.debut = form.debut.value;
+      data.fin = form.fin.value;
+   submitForm2(data);
+   }
 
 function submitForm(data){
 console.log('DATA:', data);
@@ -37,11 +56,35 @@ console.log(settings);
  
 $.ajax(settings).done(function (response) {
  console.log(response);
+
  if (response.success){
-     alert("le rapport a été généré avec succès");
-     //document.getElementById('formulaire').reset();
+    alert("le rapport inscription a été généré avec succès");
  }
  else
      alert("il n'y a pas d'informations relatives aux dates saisies.")
  });
 }
+
+function submitForm2(data){
+   console.log('DATA:', data);
+   var settings = {
+      "url": "http://localhost:3000/Programme",
+      "method": "POST",
+      "timeout": 0,
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "data": JSON.stringify(data)
+   };
+   console.log(settings);
+    
+   $.ajax(settings).done(function (response) {
+    console.log(response);
+   
+    if (response.success){
+       alert("le rapport programme a été généré avec succès");
+    }
+    else
+        alert("il n'y a pas d'informations relatives aux dates saisies.")
+    });
+   }
